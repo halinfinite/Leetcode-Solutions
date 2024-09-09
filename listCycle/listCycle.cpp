9dd732283ad95ac4c3abd1e1acd36f2787b12db8
Burak Şensoy <halincandenza452@gmail.com>
2024-09-09 21:49:47 +0300

    Added leetcode 141

---
+++ listCycle/listCycle.cpp
@@ -0,0 +1,28 @@
+#include <cstddef>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (head != NULL) {
+            ListNode* p1 = head;
+            ListNode* p2 = head;
+
+            while (p2 != NULL && p2->next != NULL) {
+                p1 = p1->next;
+                p2 = p2->next->next;
+            }
+                if (p1 == p2) {
+                    return true;
+            }
+
+        } else {
+            return false;
+        }
+    }
+};
